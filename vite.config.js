6329cc1c2aba44dcb14ebdb901e981cc6e43c812
Beverly Yip <beverly.yip.8000@gmail.com>
2026-01-15 02:28:53 -0500

    switch to docs

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: "/", // Use root path for custom domain
   assetsInclude: ["**/*.JPG", "**/*.jpg", "**/*.jpeg", "**/*.png", "**/*.gif"],
+  build: {
+    outDir: "docs", // Change from 'dist' to 'docs'
+  },
 });
