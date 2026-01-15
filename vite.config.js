dbbf7aee3de8d85516726824cd2cb484db245368
Beverly Yip <beverly.yip.8000@gmail.com>
2026-01-15 01:57:32 -0500

    setting up deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Use root path for custom domain
   assetsInclude: ["**/*.JPG", "**/*.jpg", "**/*.jpeg", "**/*.png", "**/*.gif"],
 });
